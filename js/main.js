b8210e6e5f10bc17583d8610ff46de61dc1f790a
Derrick Smith <prof_derricksmith@yahoo.com>
2021-10-16 14:41:46 -0400

    setup FAQ section

--- js/main.js
+++ js/main.js
@@ -180,4 +180,22 @@
      }, 7000)
 
 
+     /*
+        FAQ Accordion Section
+        ========================================================================== */
+     var acc = document.getElementsByClassName("accordion");
+     var i;
+
+     for (i = 0; i < acc.length; i++) {
+       acc[i].addEventListener("click", function() {
+         this.classList.toggle("active");
+         var panel = this.nextElementSibling;
+         if (panel.style.display === "block") {
+          panel.style.display = "none";
+         } else {
+          panel.style.display = "block";
+         }
+       });
+     }
+
 }(jQuery));
