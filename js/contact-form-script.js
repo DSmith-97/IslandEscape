70a5ec266f3de6a49ad1667eefe9a29763229e0a
Derrick_Smith <support@islandescapewatersports.com>
2022-01-16 19:35:35 -0500

    begin contact form

--- js/contact-form-script.js
+++ js/contact-form-script.js
@@ -1,44 +1,66 @@
-$("#contactForm").validator().on("submit", function (event) {
-    if (event.isDefaultPrevented()) {
-        // handle the invalid form...
-        formError();
-        submitMSG(false, "Did you fill in the form properly?");
-    } else {
-        // everything looks good!
-        event.preventDefault();
-        submitForm();
-    }
-});
 
 
-function submitForm(){
+
+function submit1Form(){
     // Initiate Variables With Form Content
     var name = $("#name").val();
     var email = $("#email").val();
     var msg_subject = $("#msg_subject").val();
     var message = $("#message").val();
 
-    sessionStorage.clear();
     const request = new XMLHttpRequest();
-    request.open("GET", "https://json.extendsclass.com/bin/e37662b5175b", true);
+    request.open("PATCH", "https://json.extendsclass.com/bin/e37662b5175b", true);
+    request.setRequestHeader("Security-key", "Your security key");
     request.onreadystatechange = () => {
-    	console.log(request.responseText);
     };
-    request.send();
+    request.send('{"pink": "and a new line! \n"}');
 
-    // $.ajax({
-    //     type: "PUT",
-    //     url: "https://json.extendsclass.com/bin/e37662b5175b",
-    //     data: "name=" + name + "&email=" + email + "&msg_subject=" + msg_subject + "&message=" + message,
-    //     success : function(text){
-    //         if (text == "success"){
-    //             formSuccess();
-    //         } else {
-    //             formError();
-    //             submitMSG(false,text);
-    //         }
-    //     }
-    // });
+    /* Contact Form Section
+    ===================================================== */
+
+    // get data from json store
+    function get1DataStore() {
+     var visitorList;
+     var data;
+     var index = 1;
+     $.ajax({
+       url: "https://myjson.dit.upm.es/api/bins/h4uf",
+       method: "Get",
+       success: function(dataStore){
+         data = dataStore;
+         console.log(data);
+       },
+       error: function() {
+           console.log("error");
+       },
+       complete: function() {
+           console.log("done");
+       }
+     });
+   }
+
+}
+
+// get data from json store
+function getDataStore() {
+ var visitorList;
+ var data;
+ var index = 1;
+ $.ajax({
+   url: "https://myjson.dit.upm.es/api/bins/h4uf",
+   method: "Get",
+   success: function(dataStore){
+     data = dataStore;
+     console.log(data);
+     return data;
+   },
+   error: function() {
+       console.log("error");
+   },
+   complete: function() {
+       console.log("done");
+   }
+ });
 }
 
 function formSuccess(){
@@ -46,7 +68,7 @@ function formSuccess(){
     submitMSG(true, "Message Submitted!")
 }
 
-function formError(){
+function form(){
     $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
         $(this).removeClass();
     });
@@ -60,3 +82,75 @@ function submitMSG(valid, msg){
     }
     $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
 }
+
+function saveFormData(updatedDataStore) {
+  $.ajax({
+    url:"https://myjson.dit.upm.es/api/bins/h4uf",
+    type:"PUT",
+    data: JSON.stringify(updatedDataStore),
+    contentType:"application/json; charset=utf-8",
+    dataType:"json",
+    success: function(data, textStatus, jqXHR){
+    // alert(textStatus);
+    }
+  });
+}
+
+// add new message data
+function submitForm(data) {
+  var contactList = data.contactList;
+
+  // get current form dataType
+  var savedFormData = getDataStore();
+
+  var contactName = $("#contact_name").val();
+  var contactEmail = $("#contact_email").val();
+  var contactMessage = $("#contact_message").val();
+
+  var newFormData = {
+    name: contactName,
+    email: contactEmail,
+    message: contactMessage
+  };
+
+  var contactSectionIsValid = function() {
+    if ( contactName != "" && contactEmail != "" && contactEmail != "" ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  if (contactSectionIsValid()) {
+    // contactList.push(newContact);
+    var formDataToSave = savedFormData.formData.push(newFormData);
+    // save updatedDataStore
+    saveFormData(formDataToSave);
+
+    // clear form
+    var contactName = $("input[name='Name']").val("");
+    var contactEmail = $("input[name='Email']").val("");
+    var contactMessage = $("input[name='Message']").val("");
+
+    // clean up alerts
+    $(".success-alert").show();
+    $(".success-alert").fadeOut(5000);
+    $(".w3-input").val("");
+  } else {
+    $(".fail-alert").show();
+    $(".fail-alert").fadeOut(3000);
+    $(".w3-input").val("");
+  }
+}
+
+$("#contactForm").validator().on("submit", function (event) {
+    if (event.isDefaultPrevented()) {
+        // handle the invalid form...
+        formError();
+        submitMSG(false, "Did you fill in the form properly?");
+    } else {
+        // everything looks good!
+        event.preventDefault();
+        submitForm("sample");
+    }
+});
